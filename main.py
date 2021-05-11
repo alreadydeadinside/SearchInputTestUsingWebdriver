6d3ca36e08d9555434cbad5d1fb8410905015e63
Bohdan Vakaliuk <bvakaluk@gmail.com>
2021-05-11 14:01:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ URL = "https://hotline.ua/"
 SEARCH_TEMPLATE = "Игрушка"
 
 browser = webdriver.Chrome(executable_path=PATH)
-searchTerm = "Гід покупця. Як вибрати..."
+expectedText = "Гід покупця. Як вибрати..."
 browser.get(URL)
 browser.implicitly_wait(30)
 browser.find_element_by_css_selector('[id="searchbox"]').send_keys(SEARCH_TEMPLATE)
 browser.find_element_by_css_selector('[id="searchbox"]').send_keys(Keys.ENTER)
 actualText = browser.find_element_by_css_selector('[class = "h2"]').text
-expectedText = searchTerm
 print(actualText)
 assert actualText == expectedText
 browser.close()
