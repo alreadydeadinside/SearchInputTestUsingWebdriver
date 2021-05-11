e1d746e4d0b39b0c905c5a0cf8174bb488bd88db
Bohdan Vakaliuk <bvakaluk@gmail.com>
2021-05-11 13:55:46 +0300

    Change assert

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import time 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 
 PATH = 'chromedriver.exe'
 URL = "https://hotline.ua/"
 SEARCH_TEMPLATE = "Игрушка"
 
-browser = webdriver.Chrome(executable_path = PATH)
+browser = webdriver.Chrome(executable_path=PATH)
+searchTerm = "Гід покупця. Як вибрати..."
 browser.get(URL)
 browser.implicitly_wait(30)
 browser.find_element_by_css_selector('[id="searchbox"]').send_keys(SEARCH_TEMPLATE)
 browser.find_element_by_css_selector('[id="searchbox"]').send_keys(Keys.ENTER)
-actualText = browser.find_element_by_css_selector('[class="h2"]').text
-expectedText = "Результати пошуку: товари в каталогах (14520)"
-
-assert "There no results found." not in browser.page_source
+actualText = browser.find_element_by_css_selector('[class = "h2"]').text
+expectedText = searchTerm
+print(actualText)
+assert actualText == expectedText
 browser.close()
